ee38aff4f4093aacab22cde343563f805b9a6ff5
Matthew Emes <m.emes@f5.com>
2020-11-12 10:13:05 -0800

    Refactor: CFE demos to use refactor branch

--- examples/cfe-3nic/main.tf
+++ examples/cfe-3nic/main.tf
@@ -11,7 +11,7 @@ terraform {
 
 # Create a custom CFE role for BIG-IP service account
 module "cfe_role" {
-  source      = "git::https://github.com/memes/f5-google-terraform-modules//modules/big-ip/cfe/role?ref=1.1.1"
+  source      = "git::https://github.com/memes/f5-google-terraform-modules//modules/cfe/role?enhancement/publish_bigip_module"
   target_type = "project"
   target_id   = var.project_id
   members     = [format("serviceAccount:%s", var.service_account)]
@@ -84,7 +84,7 @@ module "cfe_bucket" {
 }
 
 module "cfe" {
-  source                            = "git::https://github.com/memes/f5-google-terraform-modules//modules/big-ip/cfe?ref=1.1.1"
+  source                            = "git::https://github.com/memes/f5-google-terraform-modules//modules/cfe?ref=enhancement/publish_bigip_module"
   project_id                        = var.project_id
   num_instances                     = var.num_instances
   zones                             = [var.zone]
