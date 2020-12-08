471fa488c88455629bd9452f91cd063b27a7c1f2
Matthew Emes <m.emes@f5.com>
2020-12-08 12:14:48 -0800

    CFE role: add randomisation to generated id

--- modules/cfe-role/main.tf
+++ modules/cfe-role/main.tf
@@ -6,6 +6,15 @@ terraform {
   experiments = [variable_validation]
 }
 
+resource "random_id" "role_id" {
+  byte_length = 4
+
+  keepers = {
+    target_type = var.target_type
+    target_id   = var.target_id
+  }
+}
+
 # Create a custom project role for CFE; BIG-IP service accounts will be granted
 # this role, in addition to standard logging and monitoring roles.
 module "cfe_role" {
@@ -13,7 +22,7 @@ module "cfe_role" {
   version      = "6.4.0"
   target_level = var.target_type
   target_id    = var.target_id
-  role_id      = var.id
+  role_id      = coalesce(var.id, format("bigip_cfe_%s", random_id.role_id.hex))
   title        = var.title
   description  = <<EOD
 Allow BIG-IP Cloud Failover Extension to fully manage instance, network, route,
