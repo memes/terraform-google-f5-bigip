25d49897b29b08b1add603d9cad77e73642a5025
Matthew Emes <m.emes@f5.com>
2020-12-16 14:46:45 -0800

    Initial early setup changes

---
+++ modules/metadata/files/earlySetup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# shellcheck disable=SC1091
+#
+# This script will set BIG-IP parameters that are best set early in the onboarding
+# process
+
+
+if [ -f /config/cloud/gce/setupUtils.sh ]; then
+    . /config/cloud/gce/setupUtils.sh
+else
+    echo "${GCE_LOG_TS:+"$(date +%Y-%m-%dT%H:%M:%S.%03N%z): "}$0: ERROR: unable to source /config/cloud/gce/setupUtils.sh" >&2
+    [ -e /dev/ttyS0 ] && \
+        echo "$(date +%Y-%m-%dT%H:%M:%S.%03N%z): $0: ERROR: unable to source /config/cloud/gce/setupUtils.sh" >/dev/ttyS0
+    exit 1
+fi
+
+info "Waiting for mcpd to be ready"
+. /usr/lib/bigstart/bigip-ready-functions
+wait_bigip_ready
+
+# Provision extra RAM
+/usr/bin/setdb provision.extramb 1000 || \
+    info "Unable to provision extra RAM"
+/usr/bin/setdb restjavad.useextramb true || \
+    info "Unable to use extra RAM in restjavad"
+
+exit 0
