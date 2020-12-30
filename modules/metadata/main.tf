845e234cce4d03047b4c50a348fb23ea8815b3a5
Matthew Emes <m.emes@f5.com>
2020-12-30 11:58:14 -0800

    Chore: default_gateway applied via DO only  - remove setting default gateway via TMSH  - respect default_gateway when generating DO  - must be blank or valid IPv4

--- modules/metadata/main.tf
+++ modules/metadata/main.tf
@@ -45,7 +45,6 @@ locals {
       shutdown-script = templatefile("${path.module}/templates/shutdown_script.sh",
         {}
       )
-      default_gateway    = var.default_gateway
       install_cloud_libs = join(" ", var.install_cloud_libs)
       admin_password_key = var.admin_password_secret_manager_key
       as3_payload        = base64gzip(element(local.as3_payloads, i))
