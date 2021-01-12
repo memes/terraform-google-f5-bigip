2e34cd8118f6d0a9bef0677518586df7f6704792
Matthew Emes <m.emes@f5.com>
2021-01-12 13:13:56 -0800

    Chore: name reserved addresses similarly to instances

--- test/fixtures/cfe/addresses.tf
+++ test/fixtures/cfe/addresses.tf
@@ -4,7 +4,7 @@
 resource "google_compute_address" "alpha_private" {
   count        = var.reserve_addresses ? var.num_instances : 0
   project      = var.project_id
-  name         = format(format("%s-%s-alpha-priv", var.prefix, var.instance_name_template), count.index)
+  name         = format(format("%s-%s-alpha-priv", var.prefix, var.instance_name_template), count.index + var.instance_ordinal_offset)
   subnetwork   = var.alpha_subnet
   address_type = "INTERNAL"
   region       = var.region
@@ -13,7 +13,7 @@ resource "google_compute_address" "alpha_private" {
 resource "google_compute_address" "alpha_public" {
   count        = var.reserve_addresses && var.provision_external_public_ip ? var.num_instances : 0
   project      = var.project_id
-  name         = format(format("%s-%s-alpha-pub", var.prefix, var.instance_name_template), count.index)
+  name         = format(format("%s-%s-alpha-pub", var.prefix, var.instance_name_template), count.index + var.instance_ordinal_offset)
   address_type = "EXTERNAL"
   region       = var.region
   network_tier = var.external_subnetwork_tier
@@ -23,7 +23,7 @@ resource "google_compute_address" "alpha_public" {
 resource "google_compute_address" "beta_private" {
   count        = var.reserve_addresses && var.num_nics > 1 ? var.num_instances : 0
   project      = var.project_id
-  name         = format(format("%s-%s-beta-priv", var.prefix, var.instance_name_template), count.index)
+  name         = format(format("%s-%s-beta-priv", var.prefix, var.instance_name_template), count.index + var.instance_ordinal_offset)
   subnetwork   = var.beta_subnet
   address_type = "INTERNAL"
   region       = var.region
@@ -32,7 +32,7 @@ resource "google_compute_address" "beta_private" {
 resource "google_compute_address" "beta_public" {
   count        = var.reserve_addresses && var.provision_management_public_ip && var.num_nics > 1 ? var.num_instances : 0
   project      = var.project_id
-  name         = format(format("%s-%s-beta-pub", var.prefix, var.instance_name_template), count.index)
+  name         = format(format("%s-%s-beta-pub", var.prefix, var.instance_name_template), count.index + var.instance_ordinal_offset)
   address_type = "EXTERNAL"
   region       = var.region
   network_tier = var.management_subnetwork_tier
@@ -42,7 +42,7 @@ resource "google_compute_address" "beta_public" {
 resource "google_compute_address" "gamma_private" {
   count        = var.reserve_addresses && var.num_nics > 2 ? var.num_instances : 0
   project      = var.project_id
-  name         = format(format("%s-%s-gamma-priv", var.prefix, var.instance_name_template), count.index)
+  name         = format(format("%s-%s-gamma-priv", var.prefix, var.instance_name_template), count.index + var.instance_ordinal_offset)
   subnetwork   = var.gamma_subnet
   address_type = "INTERNAL"
   region       = var.region
@@ -51,7 +51,7 @@ resource "google_compute_address" "gamma_private" {
 resource "google_compute_address" "gamma_public" {
   count        = var.reserve_addresses && var.provision_internal_public_ip && var.num_nics > 2 ? var.num_instances : 0
   project      = var.project_id
-  name         = format(format("%s-%s-gamma-pub", var.prefix, var.instance_name_template), count.index)
+  name         = format(format("%s-%s-gamma-pub", var.prefix, var.instance_name_template), count.index + var.instance_ordinal_offset)
   address_type = "EXTERNAL"
   region       = var.region
   network_tier = var.internal_subnetwork_tier
@@ -61,7 +61,7 @@ resource "google_compute_address" "gamma_public" {
 resource "google_compute_address" "delta_private" {
   count        = var.reserve_addresses && var.num_nics > 3 ? var.num_instances : 0
   project      = var.project_id
-  name         = format(format("%s-%s-delta-priv", var.prefix, var.instance_name_template), count.index)
+  name         = format(format("%s-%s-delta-priv", var.prefix, var.instance_name_template), count.index + var.instance_ordinal_offset)
   subnetwork   = var.delta_subnet
   address_type = "INTERNAL"
   region       = var.region
@@ -70,7 +70,7 @@ resource "google_compute_address" "delta_private" {
 resource "google_compute_address" "delta_public" {
   count        = var.reserve_addresses && var.provision_internal_public_ip && var.num_nics > 3 ? var.num_instances : 0
   project      = var.project_id
-  name         = format(format("%s-%s-delta-pub", var.prefix, var.instance_name_template), count.index)
+  name         = format(format("%s-%s-delta-pub", var.prefix, var.instance_name_template), count.index + var.instance_ordinal_offset)
   address_type = "EXTERNAL"
   region       = var.region
   network_tier = var.internal_subnetwork_tier
@@ -80,7 +80,7 @@ resource "google_compute_address" "delta_public" {
 resource "google_compute_address" "epsilon_private" {
   count        = var.reserve_addresses && var.num_nics > 4 ? var.num_instances : 0
   project      = var.project_id
-  name         = format(format("%s-%s-epsilon-priv", var.prefix, var.instance_name_template), count.index)
+  name         = format(format("%s-%s-epsilon-priv", var.prefix, var.instance_name_template), count.index + var.instance_ordinal_offset)
   subnetwork   = var.epsilon_subnet
   address_type = "INTERNAL"
   region       = var.region
@@ -89,7 +89,7 @@ resource "google_compute_address" "epsilon_private" {
 resource "google_compute_address" "epsilon_public" {
   count        = var.reserve_addresses && var.provision_internal_public_ip && var.num_nics > 4 ? var.num_instances : 0
   project      = var.project_id
-  name         = format(format("%s-%s-epsilon-pub", var.prefix, var.instance_name_template), count.index)
+  name         = format(format("%s-%s-epsilon-pub", var.prefix, var.instance_name_template), count.index + var.instance_ordinal_offset)
   address_type = "EXTERNAL"
   region       = var.region
   network_tier = var.internal_subnetwork_tier
@@ -99,7 +99,7 @@ resource "google_compute_address" "epsilon_public" {
 resource "google_compute_address" "zeta_private" {
   count        = var.reserve_addresses && var.num_nics > 5 ? var.num_instances : 0
   project      = var.project_id
-  name         = format(format("%s-%s-zeta-priv", var.prefix, var.instance_name_template), count.index)
+  name         = format(format("%s-%s-zeta-priv", var.prefix, var.instance_name_template), count.index + var.instance_ordinal_offset)
   subnetwork   = var.zeta_subnet
   address_type = "INTERNAL"
   region       = var.region
@@ -108,7 +108,7 @@ resource "google_compute_address" "zeta_private" {
 resource "google_compute_address" "zeta_public" {
   count        = var.reserve_addresses && var.provision_internal_public_ip && var.num_nics > 5 ? var.num_instances : 0
   project      = var.project_id
-  name         = format(format("%s-%s-zeta-pub", var.prefix, var.instance_name_template), count.index)
+  name         = format(format("%s-%s-zeta-pub", var.prefix, var.instance_name_template), count.index + var.instance_ordinal_offset)
   address_type = "EXTERNAL"
   region       = var.region
   network_tier = var.internal_subnetwork_tier
@@ -118,7 +118,7 @@ resource "google_compute_address" "zeta_public" {
 resource "google_compute_address" "eta_private" {
   count        = var.reserve_addresses && var.num_nics > 6 ? var.num_instances : 0
   project      = var.project_id
-  name         = format(format("%s-%s-eta-priv", var.prefix, var.instance_name_template), count.index)
+  name         = format(format("%s-%s-eta-priv", var.prefix, var.instance_name_template), count.index + var.instance_ordinal_offset)
   subnetwork   = var.eta_subnet
   address_type = "INTERNAL"
   region       = var.region
@@ -127,7 +127,7 @@ resource "google_compute_address" "eta_private" {
 resource "google_compute_address" "eta_public" {
   count        = var.reserve_addresses && var.provision_internal_public_ip && var.num_nics > 6 ? var.num_instances : 0
   project      = var.project_id
-  name         = format(format("%s-%s-eta-pub", var.prefix, var.instance_name_template), count.index)
+  name         = format(format("%s-%s-eta-pub", var.prefix, var.instance_name_template), count.index + var.instance_ordinal_offset)
   address_type = "EXTERNAL"
   region       = var.region
   network_tier = var.internal_subnetwork_tier
@@ -137,7 +137,7 @@ resource "google_compute_address" "eta_public" {
 resource "google_compute_address" "theta_private" {
   count        = var.reserve_addresses && var.num_nics > 7 ? var.num_instances : 0
   project      = var.project_id
-  name         = format(format("%s-%s-theta-priv", var.prefix, var.instance_name_template), count.index)
+  name         = format(format("%s-%s-theta-priv", var.prefix, var.instance_name_template), count.index + var.instance_ordinal_offset)
   subnetwork   = var.theta_subnet
   address_type = "INTERNAL"
   region       = var.region
@@ -146,7 +146,7 @@ resource "google_compute_address" "theta_private" {
 resource "google_compute_address" "theta_public" {
   count        = var.reserve_addresses && var.provision_internal_public_ip && var.num_nics > 7 ? var.num_instances : 0
   project      = var.project_id
-  name         = format(format("%s-%s-theta-pub", var.prefix, var.instance_name_template), count.index)
+  name         = format(format("%s-%s-theta-pub", var.prefix, var.instance_name_template), count.index + var.instance_ordinal_offset)
   address_type = "EXTERNAL"
   region       = var.region
   network_tier = var.internal_subnetwork_tier
