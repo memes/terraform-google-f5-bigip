dc52a7456c6623c16c6f53c58a027a5c62fe3c24
Matthew Emes <m.emes@f5.com>
2021-04-23 10:23:05 -0700

    Fix module use on Terraform 0.15
    Closes #121

--- modules/metadata/outputs.tf
+++ modules/metadata/outputs.tf
@@ -1,5 +1,9 @@
 output "metadata" {
-  value       = local.metadata
+  value = local.metadata
+  # Metadata does not contain sensitive data, but Terraform 0.15 is overly
+  # aggressive when some functions are involved. Mark output as sensitive until
+  # https://github.com/hashicorp/terraform/issues/27337 is in a 0.15.x release.
+  sensitive   = true
   description = <<EOD
 The list of metadata maps to apply to instances.
 EOD
